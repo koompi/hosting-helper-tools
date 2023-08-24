020f7c654c8bdb82fed088fca87d401bdb4067c5
IsaacJReay <isaacjacksonreay@gmail.com>
2023-08-24 21:21:35 +0700

    use default user in NGINX

--- libnginx-wrapper/src/templates/nginx_conf.rs
+++ libnginx-wrapper/src/templates/nginx_conf.rs
@@ -1,8 +1,7 @@
 use super::super::{SPA_SITES_PATH, FILE_SITES_PATH, PROXY_SITES_PATH, STREAM_SITES_PATH, REDIRECT_SITES_PATH, PROGRAM_BASE_NAME, NGINX_DEFAULT_CERT_PATH};
 
 pub(crate) fn gen_templ() -> String {
-    format!("user http;
-worker_cpu_affinity auto;
+    format!("worker_cpu_affinity auto;
 worker_processes auto;
 pid /run/{PROGRAM_BASE_NAME}.pid;
 include /etc/{PROGRAM_BASE_NAME}/modules-enabled/*.conf;
