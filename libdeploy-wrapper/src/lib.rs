a328d8c9e1fbac8a54afe2ee882f81a28229bf11
IsaacJReay <isaacjacksonreay@gmail.com>
2023-11-28 22:46:46 +0700

    fix code redundant

--- libdeploy-wrapper/src/lib.rs
+++ libdeploy-wrapper/src/lib.rs
@@ -7,39 +7,31 @@ pub async fn init_migration() -> Result<(), (u16, String)> {
     let npm_output = tokio::spawn(Command::new("npm").arg("-v").output());
     let pnpm_output = tokio::spawn(Command::new("pnpm").arg("-v").output());
     let node_output = tokio::spawn(Command::new("node").arg("-v").output());
-    let pm2_output = tokio::spawn(Command::new("pm2").arg("-v").output());
+    let pm2_output = tokio::spawn(Command::new("pm2").arg("-v").arg("-s").output());
+    get_res(node_output.await.unwrap(), "node")?;
+    get_res(npm_output.await.unwrap(), "npm")?;
+    get_res(pnpm_output.await.unwrap(), "pnpm")?;
+    get_res(pm2_output.await.unwrap(), "pm2")?;
 
-    match npm_output.await.unwrap() {
-        Ok(output) => match output.status.success() {
-            true => Ok(println!("Your npm version is {}", String::from_utf8(output.stdout).unwrap().trim())),
-            false => Err((500, String::from("Your npm has problem. Please reinstall or recheck if usable"))),
-        },
-        Err(_) => Err((500, String::from("Your npm has problem. Please reinstall or recheck if usable")))
-    }?;
-
-    match node_output.await.unwrap() {
-        Ok(output) => match output.status.success() {
-            true => Ok(println!("Your node version is {}", String::from_utf8(output.stdout).unwrap().trim())),
-            false => Err((500, String::from("Your node has problem. Please reinstall or recheck if usable"))),
-        },
-        Err(_) => Err((500, String::from("Your node has problem. Please reinstall or recheck if usable")))
-    }?;
-
-    match pnpm_output.await.unwrap() {
-        Ok(output) => match output.status.success() {
-            true => Ok(println!("Your pnpm version is {}", String::from_utf8(output.stdout).unwrap().trim())),
-            false => Err((500, String::from("Your pnpm has problem. Please reinstall or recheck if usable"))),
-        },
-        Err(_) => Err((500, String::from("Your pnpm has problem. Please reinstall or recheck if usable")))
-    }?;
-
-    match pm2_output.await.unwrap() {
-        Ok(output) => match output.status.success() {
-            true => Ok(println!("Your pm2 version is {}", String::from_utf8(output.stdout).unwrap().trim())),
-            false => Err((500, String::from("Your pm2 has problem. Please reinstall or recheck if usable"))),
-        },
-        Err(_) => Err((500, String::from("Your pm2 has problem. Please reinstall or recheck if usable")))
-    }?;
-
+    Command::new("pm2").arg("startup").output().await.unwrap();
     Ok(())
+}
+
+fn get_res(cmd_output: Result<std::process::Output, std::io::Error>, prog: &str) -> Result<(), (u16, String)> {
+    match cmd_output {
+        Ok(output) => match output.status.success() {
+            true => Ok(println!(
+                "Your {prog} version is {}",
+                String::from_utf8(output.stdout).unwrap().trim()
+            )),
+            false => Err((
+                500,
+                format!("Your {} has problem. Please reinstall or recheck if usable", prog),
+            )),
+        },
+        Err(_) => Err((
+            500,
+            format!("Your {} has problem. Please reinstall or recheck if usable", prog),
+        )),
+    }
 }
