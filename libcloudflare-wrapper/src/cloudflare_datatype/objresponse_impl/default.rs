8656a77e40d9fef1bd2586f4ed3a4a4d051be666
IsaacJReay <isaacjacksonreay@gmail.com>
2023-12-31 18:40:17 +0700

    update new fix with warning

--- libcloudflare-wrapper/src/cloudflare_datatype/objresponse_impl/default.rs
+++ libcloudflare-wrapper/src/cloudflare_datatype/objresponse_impl/default.rs
@@ -4,18 +4,23 @@ use super::{Client, HeaderMap, ObjResponse};
 
 impl ObjResponse {
     pub fn get_client() -> Client {
-        reqwest::Client::builder().local_address("0.0.0.0".parse::<std::net::IpAddr>().unwrap()).build().unwrap()
+        reqwest::Client::builder()
+            .local_address("0.0.0.0".parse::<std::net::IpAddr>().unwrap())
+            .build()
+            .unwrap()
     }
 
     pub fn get_headers_cloudflare() -> HeaderMap {
         let mut headers = reqwest::header::HeaderMap::new();
         headers.insert("Content-Type", "application/json".parse().unwrap());
-        headers.insert(
-            "Authorization",
-            format!("Bearer {}", dotenv::var("AUTHTOKEN").unwrap())
-                .parse()
-                .unwrap(),
-        );
+        let bearer = match dotenv::var("AUTHTOKEN") {
+            Ok(data) => data,
+            Err(_) => {
+                eprintln!("Warning: Missing Bearer Authtoken\nWarning: If you're using offline without any SSL or IPCHECK or Cloudflare, Ignore this.");
+                String::new()
+            }
+        };
+        headers.insert("Authorization", bearer.parse().unwrap());
         headers
     }
 
